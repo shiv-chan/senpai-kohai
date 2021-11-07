cdf57a320b8daab4d525a7ceb9159ac163caaf20
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-11-06 17:18:31 -0700

    add models, controllers and routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import loginRoute from "./routes/logIn.js";
+import signUpRoute from "./routes/signUp.js";
 
 const app = express();
 dotenv.config();
@@ -11,6 +13,8 @@ dotenv.config();
 app.use(bodyParser.json({ limits: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limits: "30mb", extended: true }));
 app.use(cors());
+app.use("/login", loginRoute);
+app.use("/signup", signUpRoute);
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 5000; // for Heroku?
