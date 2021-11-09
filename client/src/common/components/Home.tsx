e4900b45e31b3790ade15d442f34e11ec6cbd46e
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-11-09 01:36:42 -0800

    add color theme and class for test

--- client/src/common/components/Home.tsx
+++ client/src/common/components/Home.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Home = () => {
-	return <div>This is Home</div>;
+	// added style here just to see the classes
+	return <div　className="bg-primaryPink w-screen h-screen">This is Home</div>;
 };
 
 export default Home;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      primaryPink: "#f4eeff", // main bg
+      secondaryPurple: "#a6b1e1", // secondary bg
+      lightPurple: "#dcd6f7", // dropdown menu, tech stack tags
+      darkPurple: "#424874", // headers, top titles etc
+      veryDarkPurple: "#2f2e41", // some titles
+      pinkishGray: "#d0cde1", // icons
+      ourGray: "#c4c4c4", // no img
+    }
+  },
   },
   variants: {
     extend: {},
