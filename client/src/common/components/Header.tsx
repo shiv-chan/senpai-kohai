d5cb3d358fe9dd32e184ca19f8cafe1e44bbc946
Danny <ljw5040@gmail.com>
2021-11-09 17:40:32 -0800

    made new branch and wrote some codes

--- client/src/common/components/Header.tsx
+++ client/src/common/components/Header.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 	// test tailwind
-	return <div className="bg-header_color text-white">This is Header</div>;
+	return <div className="bg-header_color text-white flex justify-between items=center p-6" >
+				<div className="flex-grow text-3xl">
+					<Link to="/">
+						Senpai-Kohai
+					</Link>
+				</div>
+				<div className="text-xl p-2 box-border border-2 rounded-full">
+					<Link to="/">Login</Link>
+				</div>
+				<div className="bg-primary_bg_color text-header_color text-xl p-2 box-border border-2 rounded-full">
+					<Link to="/">Sign up</Link>
+				</div>
+
+	</div>;
 
 };
 
