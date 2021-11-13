2b2016fa14b784b2233ed1a85086b60418bee3e2
shiv-chan <kshibuya31@gmail.com>
2021-11-13 13:31:02 -0800

    change method post to put

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -2,8 +2,8 @@ import express from 'express';
 const router = express.Router();
 import { createProfile, getProfile } from '../controllers/profile.js';
 
-router.post('/setting/senpai', createProfile('senpaiProfile'));
-router.post('/setting/kohai', createProfile('kohaiProfile'));
+router.put('/setting/senpai', createProfile('senpaiProfile'));
+router.put('/setting/kohai', createProfile('kohaiProfile'));
 router.get('/senpai', getProfile('senpaiProfile'));
 router.get('/kohai', getProfile('kohaiProfile'));
 
