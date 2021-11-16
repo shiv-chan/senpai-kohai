92afdc6704be9598ff1a6d468124c78604c60a08
shiv-chan <kshibuya31@gmail.com>
2021-11-16 09:30:10 -0800

    merge Miyabi's code manually

--- client/src/features/login/LogIn.tsx
+++ client/src/features/login/LogIn.tsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import LeftPart from '../../common/components/LeftPart';
+import axios from 'axios';
 
 const LogIn = () => {
-  return <div>This is LogIn</div>;
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+
+	const loginWithEmailAndPassword = async (
+		e: React.FormEvent<HTMLButtonElement>
+	) => {
+		e.preventDefault();
+		try {
+			console.log('clicked');
+			await axios.post(
+				'http://localhost:5000/login',
+				{
+					email,
+					password,
+				},
+				{
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}
+			);
+			console.log('logged in successfully!');
+			setEmail('');
+			setPassword('');
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<div className="flex w-screen h-screen bg-primary_bg_color text-primary_title_color">
+			<LeftPart />
+			<section className="flex justify-center my-auto h-9/12 w-1/2">
+				<div className="flex flex-col justify-end h-full w-7/12">
+					<p className="flex-initial text-4xl mb-2">Login</p>
+					<p className="flex-initial mb-12">
+						Not registered yet?{' '}
+						<Link to="/signup" className="underline">
+							Create an Account
+						</Link>
+					</p>
+					<form action="" className="flex flex-col">
+						<label htmlFor="email" className="text-lg mb-2">
+							Email Address
+						</label>
+						<input
+							type="email"
+							className="mb-8 h-10 text-xl"
+							onChange={(e) => setEmail(e.target.value)}
+							value={email}
+						/>
+						<label htmlFor="password" className="text-lg mb-2">
+							Password
+						</label>
+						<input
+							type="password"
+							className="h-10 mb-12"
+							onChange={(e) => setPassword(e.target.value)}
+							value={password}
+						/>
+						<input type="checkbox" className="p-10" />
+						<label htmlFor="">Remember me</label>
+						<Link to="/forgotpassword" className="underline">
+							Forgot password?
+						</Link>
+						<button
+							className="button"
+							onClick={(e) => loginWithEmailAndPassword(e)}
+						>
+							Login
+						</button>
+					</form>
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default LogIn;
--- client/src/features/signup/SignUp.tsx
+++ client/src/features/signup/SignUp.tsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import LeftPart from '../../common/components/LeftPart';
+import axios from 'axios';
 
 const SignUp = () => {
-  return <div>This is SignUp</div>;
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+
+	const signUpWithEmailAndPassword = async (
+		e: React.FormEvent<HTMLButtonElement>
+	) => {
+		e.preventDefault();
+		try {
+			console.log('clicked');
+			await axios.post(
+				'http://localhost:5000/signup',
+				{
+					email,
+					password,
+				},
+				{
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}
+			);
+			console.log('signed up successfully!');
+			setEmail('');
+			setPassword('');
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<div className="flex w-screen h-screen bg-primary_bg_color text-primary_title_color">
+			<LeftPart />
+			<section className="flex justify-center my-auto h-9/12 w-1/2">
+				<div className="flex flex-col justify-end h-full w-7/12">
+					<p className="flex-initial text-4xl mb-2">Create an Account</p>
+					<p className="flex-initial mb-12">
+						Already have an account?{' '}
+						<Link to="/login" className="underline">
+							Login
+						</Link>
+					</p>
+					<form action="" className="flex flex-col">
+						<label htmlFor="email" className="text-lg mb-2">
+							Email Address
+						</label>
+						<input
+							type="email"
+							className="mb-8 h-10 text-xl"
+							onChange={(e) => setEmail(e.target.value)}
+							value={email}
+						/>
+						<label htmlFor="password" className="text-lg mb-2">
+							Password
+						</label>
+						<input
+							type="password"
+							className="h-10 mb-12"
+							onChange={(e) => setPassword(e.target.value)}
+							value={password}
+						/>
+						<button
+							className="button"
+							onClick={(e) => signUpWithEmailAndPassword(e)}
+						>
+							Sign Up
+						</button>
+					</form>
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default SignUp;
