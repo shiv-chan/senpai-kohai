c49a4b78eaa161b984d256dbe42cd5e980d25c6e
shiv-chan <kshibuya31@gmail.com>
2021-11-18 23:04:19 -0800

    update jwt payload

--- server/controllers/signUp.js
+++ server/controllers/signUp.js
@@ -22,16 +22,16 @@ export const signUp_post = async (req, res) => {
 	try {
 		const user = await User.findOne({ email }).exec();
 		if (user) {
-			res.status(409).send({ message: 'This email is already registered' });
+			res.status(409).json({ message: 'This email is already registered' });
 		} else {
-			await signedUser.save();
+			const newUser = await signedUser.save();
 			// create and assign a token
-			const token = jwt.sign({ email }, process.env.TOKEN_SECRET);
+			const token = jwt.sign({ _id: newUser._id }, process.env.TOKEN_SECRET);
 			res.cookie('access_token', token, {
 				httpOnly: true,
 				// secure: true  // --> uncomment on production
 			});
-			res.status(201).json(signedUser);
+			res.status(201).json({ message: 'Signed up successfully!' });
 		}
 	} catch (error) {
 		res.status(409).json({ message: error.message });
