113160d74d6b5316dcf7f855a0457cb71d17bb15
shiv-chan <kshibuya31@gmail.com>
2021-11-24 20:54:44 -0800

    update header component

--- client/src/common/components/Header.tsx
+++ client/src/common/components/Header.tsx
@@ -1,161 +1,54 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-// import { FaTimes } from 'react-icons/fa';
-// import { FaBars } from 'react-icons/fa';
+import { Squash as Hamburger } from 'hamburger-react';
 
 const Header = () => {
-	const [isClosed, setClosed] = useState(true);
+	const [isOpen, setOpen] = useState(false);
 
-	useEffect(() => {
-		document.addEventListener('mousedown', () => {
-			setClosed(true);
-		});
-	});
-
-	const mymenu = React.useRef() as React.MutableRefObject<HTMLDivElement>;
+	const menuStyle = () => {
+		return isOpen
+			? { height: '30vh', transform: 'translateY(0%)' }
+			: { height: '30vh', transform: 'translateY(-120%)' };
+	};
 
 	return (
-		<div
-			className="bg-header_color
-						text-white flex
-						justify-between
-						items=center 
-						font-black 
-						w-screen
-						h-14
-						md:h-16
-						p-4
-						mx-auto
-						"
-		>
-			<div
-				className="flex-grow 
-							text-lg
-							w-screen
-							md:text-3xl
-							"
-			>
-				<Link to="/">Senpai-Kohai</Link>
-			</div>
-			{/* {!hasValidToken ? ( */}
-			{/* <div className="text-base 
-								box-border 
-								border-2 
-								w-20 
-								rounded-full 
-								cursor-pointer 
-								font-base 
-								hover:bg-primary_bg_color 
-								mr-5 
-								text-center">
-					<Link to="/login">Login</Link>
-				</div>
-				<div className="bg-primary_bg_color text-header_color w-20 text-base box-border border-2 rounded-full cursor-pointer hover:bg-secondary_bg_color text-center">
-					<Link to="/signup">Sign up</Link>
-				</div> */}
-			{/* ) : ( */}
-
-			<div
-				className="text-xs
-							box-border 
-							border-2 
-							md:w-40
-							p-0.5
-							md:p-0
-							w-40
-							rounded-full 
-							cursor-pointer 
-							hover:bg-primary_bg_color 
-							mr-10 
-							text-center
-							md:text-base
-							ml-6
-							"
-			>
-				Sign Out
-			</div>
-
-			<div className="">
-				<header className="bg-header_color h-10 flex items-center justify-center">
-					{isClosed ? (
-						<button
-							className="w-8 mb-4 md:w-12 md:mb-2"
-							aria-label="Open menu"
-							title="Open menu"
-							onClick={() => setClosed(false)}
-						>
-							<svg
-								aria-hidden="true"
-								fill="none"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								<path d="M4 6h16M4 12h16M4 18h16"></path>
-							</svg>
+		<>
+			<header className="bg-header_color w-screen h-mobileHeaderHeight md:h-laptopHeaderHeight fixed z-10">
+				<div className="flex items-center	font-sans text-primary_bg_color h-full mx-4">
+					<Link to="/" className="italic font-bold flex-grow self-center">
+						Senpai-Kohai
+					</Link>
+					<div className="flex items-center gap-1 relative">
+						<button className="self-center border-2 border-solid	border-primary_bg_color rounded-full px-5 text-sm	">
+							Log out
 						</button>
-					) : (
-						<button
-							className="w-8 mb-4 md:w-12 md:mb-2"
-							aria-label="Close menu"
-							title="Close menu"
-							onClick={() => setClosed(true)}
-						>
-							<svg
-								fill="none"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								<path d="M6 18L18 6M6 6l12 12"></path>
-							</svg>
-						</button>
-					)}
-				</header>
-			</div>
-			{/* )} */}
-			<div
-				className="absolute 
-							text-centet 
-							w-screen
-							-ml-4
-							"
-			>
-				{!isClosed && (
-					<div
-						ref={mymenu}
-						aria-hidden={isClosed}
-						className="
-									bg-primary_bg_color 
-									text-center 
-									mt-12 
-									absolute 
-									w-full"
-					>
-						<ul
-							className="text-header_color 
-									bg-primary_bg_color 
-									text-5x1
-									md:text-3xl"
-						>
-							<li className="p-4 md:p-8">
-								<Link to="/"> My Browsing Peeps </Link>
-							</li>
-							<li className="p-4 md:p-8">
-								<Link to="/SenpaiProfile"> My Profile as Senpai </Link>
-							</li>
-							<li className="p-4 md:p-8">
-								<Link to="/kohaiprofile"> My Profile as Kohai </Link>
-							</li>
-						</ul>
+						<Hamburger
+							toggled={isOpen}
+							toggle={setOpen}
+							size={20}
+							distance="lg"
+							rounded
+						/>
 					</div>
-				)}
+				</div>
+			</header>
+			<div
+				className="w-screen bg-tertiary_bg_color absolute top-mobileHeaderHeight flex justify-center items-center transition duration-500 ease-linear transform"
+				style={menuStyle()}
+			>
+				<ul className="text-header_color font-sans font-bold flex flex-col gap-y-4 text-xl">
+					<Link to="/board">
+						<li>Browsing Peeps</li>
+					</Link>
+					<Link to="/profile/senpai">
+						<li>My Profile as Senpai</li>
+					</Link>
+					<Link to="/profile/kohai">
+						<li>My Profile as Kohai</li>
+					</Link>
+				</ul>
 			</div>
-		</div>
+		</>
 	);
 };
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,44 +1,41 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      backgroundImage: (theme) => ({
-        'BlobL': "url('http://localhost:3000/assets/BlobL.svg')",
-        'BlobR': "url('http://localhost:3000/assets/BlobR.svg')",
-        'terms': "url('http://localhost:3000/assets/terms.svg')",
-        'shared_workspace': "url('http://localhost:3000/assets/shared_workspace.svg')",
-        'contact': "url('http://localhost:3000/assets/contact.svg')",
-      }),
-      colors: {
-      primary_bg_color: "#f4eeff", // main bg
-      secondary_bg_color: "#a6b1e1", // secondary bg
-      tertiary_bg_color: "#dcd6f7", // dropdown menu, tech stack tags
-      primary_title_color: "#424874", // titles 
-      header_color: "#424874", // header
-      bolbR:"DCD6F7", // bolbR color
-    },
-      fontSize: {
-
-      },
-      spacing: {
+	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			backgroundImage: (theme) => ({
+				BlobL: "url('http://localhost:3000/assets/BlobL.svg')",
+				BlobR: "url('http://localhost:3000/assets/BlobR.svg')",
+				terms: "url('http://localhost:3000/assets/terms.svg')",
+				shared_workspace:
+					"url('http://localhost:3000/assets/shared_workspace.svg')",
+				contact: "url('http://localhost:3000/assets/contact.svg')",
+			}),
+			colors: {
+				primary_bg_color: '#f4eeff', // main bg
+				secondary_bg_color: '#a6b1e1', // secondary bg
+				tertiary_bg_color: '#dcd6f7', // dropdown menu, tech stack tags
+				primary_title_color: '#424874', // titles
+				header_color: '#424874', // header
+			},
+			fontSize: {},
+			spacing: {
 				// height from the main content to the bottom of Header
 				// for now 3rem but we can change it anytime
 				mobileHeaderHeight: '3.5rem',
 				laptopHeaderHeight: '4rem',
 				paddingAroundtheContent: '2.5rem',
 			},
-      
-    screens: {
-      mobile_s: '320px',
-      mobile_m: '375px',
-      mobile_l: '425px',
-    },
-  },
 
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+			screens: {
+				mobile_s: '320px',
+				mobile_m: '375px',
+				mobile_l: '425px',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
