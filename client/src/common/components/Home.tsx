0507a8607cfbd2bbfbea3ddcf392a9e0a525befd
shiv-chan <kshibuya31@gmail.com>
2021-11-29 23:18:26 -0800

    style for mobile devices

--- client/src/common/components/Home.tsx
+++ client/src/common/components/Home.tsx
@@ -13,7 +13,7 @@ import 'react-accessible-accordion/dist/fancy-example.css';
 const Home = () => {
 	// test, added style here just to see the classes work
 	return (
-		<div className="bg-primary_bg_color text-primary_title_color bg-gradient">
+		<div className="bg-primary_bg_color text-primary_title_color font-sans overflow-x-hidden">
 			<div className="p-10 md:p-20">
 				<div className="md:float-right w:20 md:w-2/4 ">
 					<img src="./assets/shared_workspace.svg" />
@@ -50,21 +50,32 @@ const Home = () => {
 					</div>
 				</div>
 			</div>
-			<div className="text-center p-10 md:p-20 bg-BlobL bg-no-repeat bg-contain">
-				<div className="text-3xl p-10 md:p-20 font-black md:text-7xl">
+			<section className="p-10 md:p-20 relative">
+				<h2 className="text-2xl md:text-5xl text-center font-bold mb-6">
 					About
-				</div>
-				<div className="text-xl md:text-3xl">
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,{' '}
+				</h2>
+				<div className="text-md md:text-xl max-w-screen-lg mx-auto relative z-10">
+					<p className="mb-4">
+						Volutpat enim commodo condimentum diam pellentesque egestas.
 					</p>
-					dolorum! Iste assumenda molestias, rerum provident eos consequatur
-					voluptatibus aut repudiandae? Hic repudiandae eos eveniet amet sit
-					architecto nostrum vitae ipsam.
+					<p className="mb-4">
+						Ut tellus viverra amet, ullamcorper mattis eget proin feugiat.
+						Suspendisse diam egestas vulputate tristique amet convallis. Ipsum
+						viverra sagittis fusce interdum dis.
+					</p>
+					<p className="mb-4">Dolor cursus viverra tincidunt sed.</p>
 				</div>
-				<br />
-				<br />
-			</div>
+				<img
+					src="./assets/BlobL.svg"
+					alt="blob-l"
+					className="absolute w-48 h-48 top-1/3 -left-14"
+				/>
+				<img
+					src="./assets/BlobR.svg"
+					alt="blob-r"
+					className="absolute w-48 h-48 top-1/4 -right-14"
+				/>
+			</section>
 			<div className="p-10 md:p-20 bg-BlobR bg-no-repeat bg-right bg-contain ">
 				<div className="text-3xl font-black text-center md:text-7xl">Q&As</div>
 				<Accordion className="p-10 text-sm md:text-2xl w-auto">
