f3dbde3425c73926cbc035d9c59fa4b3e0ef1e8c
Danny <ljw5040@gmail.com>
2021-11-30 16:46:29 -0800

    deleted all float things and used grid for positon and changed some code for responsive design. added alt for images. made section tag for about QnAs and contact.

--- client/src/common/components/Home.tsx
+++ client/src/common/components/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaQuestionCircle } from 'react-icons/fa';
 
 import {
 	Accordion,
@@ -13,135 +14,148 @@ import 'react-accessible-accordion/dist/fancy-example.css';
 const Home = () => {
 	// test, added style here just to see the classes work
 	return (
-		<div className="bg-primary_bg_color text-primary_title_color bg-gradient">
-			<div className="p-10 md:p-20">
-				<div className="md:float-right w:20 md:w-2/4 ">
-					<img src="./assets/shared_workspace.svg" />
-				</div>
-
-				<div className="text-3xl font-black md:text-7xl mt-5 text-center md:text-justify">
+		<div className="bg-primary_bg_color text-primary_title_color">
+			<div className="p-10 md:p-20 md:grid md:grid-cols-7 md:grid-row-4 grid justify-items-stretch">
+				<h1 className="text-3xl font-black md:text-7xl mt-5 text-center md:text-left col-start-1 col-span-3">
 					Senpai Kohai
-				</div>
-				<div className="mt-5 font-extrabold text-base w-auto md:text-2xl text-center md:text-justify">
+				</h1>
+				<img
+					className="md:w-6/6 md:row-span-4 md:col-span-4 mt-5"
+					src="./assets/shared_workspace.svg"
+					alt="shared_workspace"
+				/>
+				<p className="mt-5 font-extrabold text-base w-auto md:text-2xl text-center md:text-left col-span-3">
 					Find your guide, Support youngs
-				</div>
-				<div className="underline cursor-pointer mt-5 text-sm md:text-xl text-center md:text-justify">
-					<span className="">What are Senpai and Kohai?</span>
-				</div>
-				<div className="flex justify-center items-center md:justify-start">
-					<div
-						className="cursor-pointer 
+				</p>
+				<p className="underline cursor-pointer mt-5 w-auto text-sm md:text-lg text-center md:text-left flex col-span-3 justify-self-center md:justify-self-start">
+					<FaQuestionCircle />
+					&nbsp; What are Senpai and Kohai?
+				</p>
+				<div
+					className="cursor-pointer 
 								mt-5 
 								border-2 
-								w-24 
+								py-2
+								md:py-1.5
+								w-28
+								h-10 
 								text-primary_bg_color 
 								bg-primary_title_color 
 								rounded-full 
-								py-3 
 								font-black 
 								hover:bg-secondary_bg_color 
 								text-center
 								text-sm
-								justify-center
 								md:text-base
+								col-end-2
+								justify-self-center
+								md:justify-self-start
 								"
-					>
-						<Link to="/signup">Join Now</Link>
-					</div>
+				>
+					<Link to="/signup">Join Now</Link>
 				</div>
 			</div>
-			<div className="text-center p-10 md:p-20 bg-BlobL bg-no-repeat bg-contain">
-				<div className="text-3xl p-10 md:p-20 font-black md:text-7xl">
-					About
-				</div>
-				<div className="text-xl md:text-3xl">
-					<p>
+			<section>
+				<div className="text-center p-10 md:p-20">
+					<h2 className="text-3xl p-10 md:p-20 font-black md:text-7xl">
+						About
+					</h2>
+					<p className="text-xl md:text-3xl">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,{' '}
+						dolorum! Iste assumenda molestias, rerum provident eos consequatur
+						voluptatibus aut repudiandae? Hic repudiandae eos eveniet amet sit
+						architecto nostrum vitae ipsam.
 					</p>
-					dolorum! Iste assumenda molestias, rerum provident eos consequatur
-					voluptatibus aut repudiandae? Hic repudiandae eos eveniet amet sit
-					architecto nostrum vitae ipsam.
-				</div>
-				<br />
-				<br />
-			</div>
-			<div className="p-10 md:p-20 bg-BlobR bg-no-repeat bg-right bg-contain ">
-				<div className="text-3xl font-black text-center md:text-7xl">Q&As</div>
-				<Accordion className="p-10 text-sm md:text-2xl w-auto">
-					<AccordionItem className="p-5 bg-red">
-						<AccordionItemHeading>
-							<AccordionItemButton>
-								Who is Senpai for Kohai ?
-							</AccordionItemButton>
-						</AccordionItemHeading>
-						<AccordionItemPanel className="bg-primary_bg_color text-primary_title_color">
-							<p>
-								dolorum! Iste assumenda molestias, rerum provident eos
-								consequatur voluptatibus aut repudiandae? Hic repudiandae eos
-								eveniet amet sit architecto nostrum vitae ipsam.
-							</p>
-						</AccordionItemPanel>
-					</AccordionItem>
-					<AccordionItem className="p-5">
-						<AccordionItemHeading>
-							<AccordionItemButton>Why you need Senpai ?</AccordionItemButton>
-						</AccordionItemHeading>
-						<AccordionItemPanel className="bg-primary_bg_color text-primary_title_color">
-							<p>
-								dolorum! Iste assumenda molestias, rerum provident eos
-								consequatur voluptatibus aut repudiandae? Hic repudiandae eos
-								eveniet amet sit architecto nostrum vitae ipsam. dolorum! Iste
-								assumenda molestias, rerum provident eos consequatur
-								voluptatibus aut repudiandae? Hic repudiandae eos eveniet amet
-								sit architecto nostrum vitae ipsam.
-							</p>
-						</AccordionItemPanel>
-					</AccordionItem>
-					<AccordionItem className="p-5">
-						<AccordionItemHeading>
-							<AccordionItemButton>How does it work ?</AccordionItemButton>
-						</AccordionItemHeading>
-						<AccordionItemPanel className="bg-primary_bg_color text-primary_title_color">
-							<p>
-								Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
-								occaecat ut occaecat consequat est minim minim esse tempor
-								laborum consequat esse adipisicing eu reprehenderit enim.
-							</p>
-						</AccordionItemPanel>
-					</AccordionItem>
-					<AccordionItem className="p-5">
-						<AccordionItemHeading>
-							<AccordionItemButton>
-								What you can do for Kohais ?
-							</AccordionItemButton>
-						</AccordionItemHeading>
-						<AccordionItemPanel className="bg-primary_bg_color text-primary_title_color">
-							<p>
-								Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
-								occaecat ut occaecat consequat est minim minim esse tempor
-								laborum consequat esse adipisicing eu reprehenderit enim.
-								dolorum! Iste assumenda molestias, rerum provident eos
-								consequatur voluptatibus aut repudiandae? Hic repudiandae eos
-								eveniet amet sit architecto nostrum vitae ipsam.
-							</p>
-						</AccordionItemPanel>
-					</AccordionItem>
-				</Accordion>
-			</div>
-			<div className="p-10 md:p-20">
-				<div className="text-3xl md:text-7xl font-black text-center">
-					Contact
-				</div>
-				<div className="md:flex p-10 md:p-20 justify-center items-center">
-					<div className="md:w-2/4 ms:p-20">
-						<img src="./assets/contact.svg" />
+					<div className="p-10 md:p-20">
+						<h2 className="text-3xl font-black text-center md:text-7xl">
+							Q&As
+						</h2>
+						<Accordion className="p-10 text-sm md:text-2xl w-auto">
+							<AccordionItem className="p-5">
+								<AccordionItemHeading>
+									<AccordionItemButton>
+										Who is Senpai for Kohai ?
+									</AccordionItemButton>
+								</AccordionItemHeading>
+								<AccordionItemPanel>
+									<p>
+										dolorum! Iste assumenda molestias, rerum provident eos
+										consequatur voluptatibus aut repudiandae? Hic repudiandae
+										eos eveniet amet sit architecto nostrum vitae ipsam.
+									</p>
+								</AccordionItemPanel>
+							</AccordionItem>
+							<AccordionItem className="p-5">
+								<AccordionItemHeading>
+									<AccordionItemButton>
+										Why you need Senpai ?
+									</AccordionItemButton>
+								</AccordionItemHeading>
+								<AccordionItemPanel>
+									<p>
+										dolorum! Iste assumenda molestias, rerum provident eos
+										consequatur voluptatibus aut repudiandae? Hic repudiandae
+										eos eveniet amet sit architecto nostrum vitae ipsam.
+										dolorum! Iste assumenda molestias, rerum provident eos
+										consequatur voluptatibus aut repudiandae? Hic repudiandae
+										eos eveniet amet sit architecto nostrum vitae ipsam.
+									</p>
+								</AccordionItemPanel>
+							</AccordionItem>
+							<AccordionItem className="p-5">
+								<AccordionItemHeading>
+									<AccordionItemButton>How does it work ?</AccordionItemButton>
+								</AccordionItemHeading>
+								<AccordionItemPanel>
+									<p>
+										Exercitation in fugiat est ut ad ea cupidatat ut in
+										cupidatat occaecat ut occaecat consequat est minim minim
+										esse tempor laborum consequat esse adipisicing eu
+										reprehenderit enim.
+									</p>
+								</AccordionItemPanel>
+							</AccordionItem>
+							<AccordionItem className="p-5">
+								<AccordionItemHeading>
+									<AccordionItemButton>
+										What you can do for Kohais ?
+									</AccordionItemButton>
+								</AccordionItemHeading>
+								<AccordionItemPanel>
+									<p>
+										Exercitation in fugiat est ut ad ea cupidatat ut in
+										cupidatat occaecat ut occaecat consequat est minim minim
+										esse tempor laborum consequat esse adipisicing eu
+										reprehenderit enim. dolorum! Iste assumenda molestias, rerum
+										provident eos consequatur voluptatibus aut repudiandae? Hic
+										repudiandae eos eveniet amet sit architecto nostrum vitae
+										ipsam.
+									</p>
+								</AccordionItemPanel>
+							</AccordionItem>
+						</Accordion>
 					</div>
-					<div className="md:p-20 md:mt-40  mt-10 text-xl md:text-2xl text-center md:text-start">
-						<p>Please reach at xxxx@xxxx.com</p>
-						<p>Thank you!</p>
+					<div className="p-10">
+						<h2 className="text-3xl md:text-7xl font-black text-center">
+							Contact
+						</h2>
+						<div className="md:flex md:p-10 md:mt-20 flex-1 justify-items-stretch">
+							<img
+								className="md:w-2/4 ms:p-20 justify-self-center"
+								src="./assets/contact.svg"
+								alt="contact"
+							/>
+
+							<div className="text-center mt-10 md:mt-15 md:p-10  text-base md:text-5xl justify-self-center">
+								<p>
+									Please reach at xxxx@xxxx.com <br />
+									Thank you!
+								</p>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
+			</section>
 		</div>
 	);
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,18 +3,10 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
-			backgroundImage: (theme) => ({
-				BlobL: "url('http://localhost:3000/assets/BlobL.svg')",
-				BlobR: "url('http://localhost:3000/assets/BlobR.svg')",
-				terms: "url('http://localhost:3000/assets/terms.svg')",
-				shared_workspace:
-					"url('http://localhost:3000/assets/shared_workspace.svg')",
-				contact: "url('http://localhost:3000/assets/contact.svg')",
-			}),
 			colors: {
 				primary_bg_color: '#f4eeff', // main bg
 				secondary_bg_color: '#a6b1e1', // secondary bg
-				tertiary_bg_color: '#dcd6f7', // dropdown menu, tech stack tags
+				tertiary_bg_color: '#dcd6f7', // dropdown menu, tech stack tags, blobR
 				primary_title_color: '#424874', // titles
 				header_color: '#424874', // header
 			},
