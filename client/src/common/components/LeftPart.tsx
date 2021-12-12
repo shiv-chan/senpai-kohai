ac96c8880186966cdff9b6f8eb41c84e1911044c
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-12-11 16:28:52 -0800

    remove unnecessary useEffect

--- client/src/common/components/LeftPart.tsx
+++ client/src/common/components/LeftPart.tsx
@@ -1,32 +1,28 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 const LeftPart: React.FC = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    return;
-  }, [location.pathname]);
+  const { pathname } = useLocation();
 
   return (
     <section className="flex flex-col justify-center items-center w-1/2 h-full bg-white  tablet_l_max:hidden">
       <div className="h-9/12">
-        {location.pathname.includes('/forgotpassword') ? (
+        {pathname.includes('/forgotpassword') ? (
           <p className="flex-initial text-5xl mb-16">Come Back Now!</p>
         ) : (
           <p className="flex-initial text-5xl mb-16">Let's Get Started</p>
         )}
-        {location.pathname === '/signup' && (
+        {pathname === '/signup' && (
           <img src="/assets/terms.svg" alt="terms" className="w-80 mb-16" />
         )}
-        {location.pathname === '/login' && (
+        {pathname === '/login' && (
           <img
             src="/assets/shared_workspace.svg"
             alt="terms"
             className="w-80 mb-16"
           />
         )}
-        {location.pathname.includes('/forgotpassword') && (
+        {pathname.includes('/forgotpassword') && (
           <img src="/assets/password.svg" alt="terms" className="w-80 mb-16" />
         )}
         <p className="flex-initial text-2xl">Find your guide, Support youngs</p>
