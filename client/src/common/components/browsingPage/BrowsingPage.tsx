008235043c0ae416a1dd80ca74e6b9b59887856e
shiv-chan <kshibuya31@gmail.com>
2021-12-15 12:46:35 -0800

    play the code

--- client/src/common/components/browsingPage/BrowsingPage.tsx
+++ client/src/common/components/browsingPage/BrowsingPage.tsx
@@ -3,46 +3,90 @@ import { useAppSelector } from '../../../app/hook';
 import UserCard from './UserCard';
 
 const BrowsingPage = () => {
-  const users = useAppSelector((state) => state.users.users);
-  const myProfile = useAppSelector((state) => state.myProfile.myProfile);
-  const [otherUsers, setOtherUsers] = useState([]);
+	const users = useAppSelector((state) => state.users.users);
+	const myProfile = useAppSelector((state) => state.myProfile.myProfile);
+	const [otherUsers, setOtherUsers] = useState<any>([]);
+	const [senpaiProfiles, setSenpaiProfiles] = useState<any>([]);
+	const [kohaiProfiles, setKohaiProfiles] = useState<any>([]);
 
-  const createOtherUsersList = () => {
-    const otherUsersList = users.filter(
-      (user: any) => user._id !== myProfile._id
-    );
-    setOtherUsers(otherUsersList);
-    console.log(otherUsersList);
-  };
+	const createOtherUsersList = () => {
+		const otherUsersList = users.filter(
+			(user: any) => user._id !== myProfile._id
+		);
+		setOtherUsers(otherUsersList);
+		console.log(otherUsersList);
+	};
 
-  useEffect(() => {
-    if (users && myProfile) createOtherUsersList();
-  }, [users]);
+	const sortSenpai = () => {
+		if (otherUsers.length) {
+			const senpaiProfilesArr = otherUsers.map((user: any) => {
+				const { _id, name, profileImage, publicEmail, senpaiProfile } = user;
+				return {
+					id: `s${_id}`,
+					name,
+					profileImage,
+					publicEmail,
+					profile: senpaiProfile,
+					isSenapi: true,
+				};
+			});
 
-  return (
-    <>
-      {otherUsers.length !== 0 && (
-        <main className="bg-primary_bg_color flex flex-row flex-wrap justify-center items-center  mt-laptopHeaderHeight tablet_md_max:mt-mobileHeaderHeight px-14 py-16 w-full gap-6">
-          {otherUsers.map((user: any) => (
-            <>
-              <UserCard
-                {...user}
-                key={user.senpaiProfile.id}
-                profile={user.senpaiProfile}
-                isSenpai={true}
-              />
-              <UserCard
-                {...user}
-                key={user.kohaiProfile.id}
-                profile={user.kohaiProfile}
-                isSenpai={false}
-              />
-            </>
-          ))}
-        </main>
-      )}
-    </>
-  );
+			setSenpaiProfiles(senpaiProfilesArr);
+		}
+	};
+
+	const sortKohai = () => {
+		if (otherUsers.length) {
+			const kohaiProfilesArr = otherUsers.map((user: any) => {
+				const { name, profileImage, publicEmail, kohaiProfile } = user;
+				return { name, profileImage, publicEmail, profile: kohaiProfile };
+			});
+
+			setKohaiProfiles(kohaiProfilesArr);
+		}
+	};
+
+	useEffect(() => {
+		if (users && myProfile) {
+			createOtherUsersList();
+		}
+		sortSenpai();
+		sortKohai();
+	}, [users]);
+
+	console.group('Senapi');
+	console.log(senpaiProfiles);
+	console.groupEnd();
+	console.group('Kohai');
+	console.log(kohaiProfiles);
+	console.groupEnd();
+
+	return (
+		<>
+			{otherUsers.length !== 0 && (
+				<main className="bg-primary_bg_color mt-laptopHeaderHeight tablet_md_max:mt-mobileHeaderHeight px-14 py-16 w-full">
+					<section className="relative mx-auto grid grid-cols-1 md:grid-cols-2 auto-rows-auto justify-items-center items-center gap-6">
+						{otherUsers.map((user: any, index: number) => (
+							<React.Fragment key={index}>
+								<UserCard
+									{...user}
+									key={user.senpaiProfile.id}
+									profile={user.senpaiProfile}
+									isSenpai={true}
+								/>
+								<UserCard
+									{...user}
+									key={user.kohaiProfile.id}
+									profile={user.kohaiProfile}
+									isSenpai={false}
+								/>
+							</React.Fragment>
+						))}
+					</section>
+				</main>
+			)}
+		</>
+	);
 };
 
 export default BrowsingPage;
--- client/src/common/components/browsingPage/UserCard.tsx
+++ client/src/common/components/browsingPage/UserCard.tsx
@@ -2,83 +2,84 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import IUserProps from '../../../interfaces/browsing';
 import {
-  FaUserNinja,
-  MdEmail,
-  RiUser3Fill,
-  RiHeart3Line,
-  RiHeart3Fill,
+	FaUserNinja,
+	MdEmail,
+	RiUser3Fill,
+	RiHeart3Line,
+	RiHeart3Fill,
 } from 'react-icons/all';
 
 const UserCard = ({
-  name,
-  publicEmail,
-  profileImage,
-  profile,
-  isSenpai,
+	name,
+	publicEmail,
+	profileImage,
+	profile,
+	isSenpai,
 }: IUserProps) => {
-  const [isLiked, setIsLiked] = useState<boolean>(false);
+	const [isLiked, setIsLiked] = useState<boolean>(false);
 
-  const profileNameDisplay = () => {
-    if (name === '') {
-      return isSenpai
-        ? `Senpai#${profile.id.slice(0, 5)}`
-        : `Kohai#${profile.id.slice(0, 5)}`;
-    } else {
-      return name;
-    }
-  };
-  return (
-    <Link
-      to={`/profile/${isSenpai ? 'senpai' : 'kohai'}/${profile.id}`}
-      className="z-0"
-    >
-      <div className="mobile_l_max:h-48 mobile_l_max:w-72 mobile_l_max:p-2.5 h-56 w-80 mobile_l:h-64 mobile_l:w-96 flex-initial bg-white hover:bg-gray-50 rounded-lg p-5 z-0">
-        <div className="flex justify-between items-start mb-7 mb-l-max:mb-2">
-          <img
-            src={profileImage}
-            alt={`${name}'s image`}
-            className="h-28 rounded-full mr-5 mobile_l_max:h-20"
-          />
-          <div className="mt-2 w-60 mobile_l_max:w-50">
-            <div className="flex items-center justify-between mb-1">
-              <h1 className="text-xl font-bold mobile_l_max:text-base">
-                {profileNameDisplay()}
-              </h1>
-              {isLiked ? (
-                <RiHeart3Fill
-                  className="text-2xl mobile_l_max:text-xl z-30 text-favorite_color"
-                  onClick={() => setIsLiked((prevState) => !prevState)}
-                />
-              ) : (
-                <RiHeart3Line
-                  className="text-2xl mobile_l_max:text-xl z-30"
-                  onClick={() => setIsLiked((prevState) => !prevState)}
-                />
-              )}
-            </div>
-            <div className="flex items-center gap-x-1 text-sm mobile_l_max:text-xs">
-              {isSenpai ? <FaUserNinja /> : <RiUser3Fill />}
-              <p>{isSenpai ? 'Senpai' : 'Kohai'}</p>
-            </div>
-            <div className="flex items-center gap-x-1 text-sm mobile_l_max:text-xs">
-              <MdEmail />
-              <p>{publicEmail ? publicEmail : 'Contact is not provided yet'}</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-wrap">
-          {profile.techStack.map((techStack) => (
-            <span
-              key={techStack}
-              className="bg-tertiary_bg_color m-1 px-1.5 rounded-full mobile_l_max:text-xs"
-            >
-              {techStack}
-            </span>
-          ))}
-        </div>
-      </div>
-    </Link>
-  );
+	const profileNameDisplay = () => {
+		if (name === '') {
+			return isSenpai
+				? `Senpai#${profile.id.slice(0, 5)}`
+				: `Kohai#${profile.id.slice(0, 5)}`;
+		} else {
+			return name;
+		}
+	};
+	return (
+		<Link
+			to={`/profile/${isSenpai ? 'senpai' : 'kohai'}/${profile.id}`}
+			className="z-0 w-full h-full"
+		>
+			<div className="w-full h-full flex-initial bg-white hover:bg-gray-50 rounded-lg p-5 z-0">
+				<div className="flex justify-between items-start mb-7 mb-l-max:mb-2">
+					<img
+						src={profileImage}
+						alt={`${name}'s image`}
+						className="h-28 rounded-full mr-5 mobile_l_max:h-20"
+					/>
+					<div className="mt-2 w-60 mobile_l_max:w-50">
+						<div className="flex items-center justify-between mb-1">
+							<h1 className="text-xl font-bold mobile_l_max:text-base">
+								{profileNameDisplay()}
+							</h1>
+							{isLiked ? (
+								<RiHeart3Fill
+									className="text-2xl mobile_l_max:text-xl z-30 text-favorite_color"
+									onClick={() => setIsLiked((prevState) => !prevState)}
+								/>
+							) : (
+								<RiHeart3Line
+									className="text-2xl mobile_l_max:text-xl z-30"
+									onClick={() => setIsLiked((prevState) => !prevState)}
+								/>
+							)}
+						</div>
+						<div className="flex items-center gap-x-1 text-sm mobile_l_max:text-xs">
+							{isSenpai ? <FaUserNinja /> : <RiUser3Fill />}
+							<p>{isSenpai ? 'Senpai' : 'Kohai'}</p>
+						</div>
+						<div className="flex items-center gap-x-1 text-sm mobile_l_max:text-xs">
+							<MdEmail />
+							<p>{publicEmail ? publicEmail : 'Contact is not provided yet'}</p>
+						</div>
+					</div>
+				</div>
+				<div className="flex flex-wrap items-center">
+					{profile.techStack.slice(0, 5).map((techStack) => (
+						<span
+							key={techStack}
+							className="bg-tertiary_bg_color m-1 px-1.5 rounded-full mobile_l_max:text-xs"
+						>
+							{techStack}
+						</span>
+					))}
+					{profile.techStack.length > 5 ? <span> and more</span> : null}
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 export default UserCard;
